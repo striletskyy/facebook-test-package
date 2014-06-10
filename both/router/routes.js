a09c30fdb54224a1d96f8411359d522b8ffe2eb0
Mykola <striletskyy@hotmail.com>
2014-06-10 23:57:21 +0300

    Make refactoring to post and feeds

--- both/router/routes.js
+++ both/router/routes.js
@@ -9,8 +9,17 @@ Router.configure({
   routeControllerNameConverter: 'upperCamelCase'
 });
 Router.map(function () {
-  this.route('home', {path: '/'});
-  this.route('facebook', {
-   path: '/facebook'
-  });
+    this.route('home', {path: '/'});
+    this.route('facebook', {
+        path: '/facebook/feeds',
+        yieldTemplates: {
+            'Feeds': {to: 'body'}
+        }
+    });
+    this.route('facebook', {
+        path: '/facebook/post',
+        yieldTemplates: {
+            'Post': {to: 'body'}
+        }
+    });
 });
